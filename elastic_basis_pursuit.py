6f330ad943fb977f8c968947bb0373b08e49aced
arokem <arokem@gmail.com>
2014-06-13 18:32:02 -0700

    Much more work towards the full implementation.
    Putting in place some generic oracle functions, optimization, etc.

--- elastic_basis_pursuit.py
+++ elastic_basis_pursuit.py
@@ -4,8 +4,145 @@ Elastic basis pursuit
 
 import numpy as np
 import numpy.linalg as nla
+import leastsqbound as lsq
+import sklearn.linear_model as lm
 
-def elastic_basis_pursuit(x, y, oracle, func, p_xval=0.1):
+def err_func(params, x, y, func):
+        """
+        Error function for fitting a function
+        
+        Parameters
+        ----------
+        params : tuple
+            A tuple with the parameters of `func` according to their order of
+            input
+
+        x : float array 
+            An independent variable. 
+        
+        y : float array
+            The dependent variable. 
+        
+        func : function
+            A function with inputs: `(x, *params)`
+        
+        Returns
+        -------
+        The sum of squared marginals of the fit to x/y given the params
+        """
+        # We ravel both, so that we can accomodate multi-d input without having
+        # to think about it:
+        return np.ravel(y) - np.ravel(func(x, params))
+
+
+def gaussian_kernel(x, params):
+    """
+    A multi-dimensional Gaussian kernel function
+
+    Useful for creating and testing EBP with simple Gaussian Mixture Models
+
+    Parameters
+    ----------
+    x : ndarray
+        The independent variable over which the Gaussian is calculated
+
+    params : ndarray
+       If this is a 1D array, it could have one of few things:
+
+       [mu_1, mu_2, ... mu_n, sigma_1, sigma_2, ... sigma_n]
+
+       Or:
+       [mu_1, mu_2, ... mu_n, var_covar_matrix]
+
+       where: 
+
+       var_covar_matrix needs to be reshaped into n-by-n 
+       
+    """
+    if len(params) == x.shape[0] * 2:
+        mu = params[:x.shape[0]]
+        sigma = np.diag(params[x.shape[0]:])
+    elif len(params) == x.shape[0] + x.shape[0] ** 2:
+        mu = params[:x.shape[0]]
+        sigma = np.reshape(params[x.shape[0]:], (x.shape[0], x.shape[0]))
+    else:
+        e_s = "Inputs to gaussian_kernel don't have the right dimensions"
+        raise ValueError(e_s)
+
+    dims = mu.shape[0]
+    while len(mu.shape) < len(x.shape): 
+        mu = mu[..., None]
+
+    shape_tuple = x.shape[1:]
+    diff = (x - mu).reshape(x.shape[0], -1)
+    sigma_inv = nla.inv(sigma)
+    mult1 = np.dot(diff.T, sigma_inv)
+    mult2 =  (np.diag(np.dot(mult1, diff))).reshape(shape_tuple)
+    norm_factor = 1/(np.sqrt((2*np.pi)**dims * nla.det(sigma)))
+    gauss = norm_factor * np.exp(-0.5 * mult2) 
+    return gauss
+
+
+def leastsq_oracle(x, y, func, initial, bounds=None):
+    """
+    This is a generic oracle function that uses bounded least squares to find
+    the parameters in each iteration of EBP, and requires initial parameters. 
+
+    Parameters
+    ----------
+    x : ndarray
+        Input to the kernel function.
+    y : ndarray
+        Data to fit to.
+    func : callalble
+       The kernel function to be specified by this oracle.
+    initial : list/array
+        initial setting for the parameters of the function. This has to be
+        something that func knows what to do with.
+    """    
+    return lsq.leastsqbound(err_func, initial, args=(x, y, func),
+                            bounds=bounds)[0]
+
+
+def mixture_of_kernels(x, betas, params, kernel):
+    """
+
+    Generate the signal from a mixture of kernels
+
+    Parameters
+    ----------
+    x : ndarray
+
+    betas : 1D array
+        Coefficients for the linear summation of the kernels
+
+    params : list
+        A set of parameters for each one of the kernels 
+
+    kernel : callable
+    
+    """
+    out = np.zeros(len(betas), x.ravel().shape)
+    for i in xrange(out.shape[0]):
+        out[i] = np.dot(betas[i], kernel(x, params[i]))
+
+def kernel_err(y, x, betas, params, kernel):
+    """
+    An error function for a mixture of kernels, each one parameterized by its
+    own set of params, and weighted by a beta
+
+
+    Note
+    ----
+    For a given set of betas, params, this can be used as a within set error
+    function, or to estimate the cross-validation error against another set of
+    y, x values, sub-sampled from the whole original set, or from a left-out
+    portion
+    """
+    return y - mixture_of_kernels(x, betas, params, kernel)
+    
+     
+def elastic_basis_pursuit(x, y, oracle, func):
     """
     Elastic basis pursuit
 
@@ -20,14 +157,13 @@ def elastic_basis_pursuit(x, y, oracle, func, p_xval=0.1):
 
     Parameters
     ----------
-    x : ndarray
+    x : 1D/2D array
         The independent variable that produces the data
 
-    y : ndarray
+    y : 1D/2D darray
         The data to be fit.
 
     oracle : callable
-
         This is a function that takes data (`y`) and an input function (`func`)
         and returns another callable that when called with the i-th item in x
         will produce something that has the dimensions of the i-th item in y.
@@ -37,47 +173,18 @@ def elastic_basis_pursuit(x, y, oracle, func, p_xval=0.1):
         of the dimensions of x (together with params, and with args) and return
         something of the dimensions of y.  
         
-    """
+    """    
+    r = y
 
+    # Find the first function:
+    theta0 = oracle(x, y)
+    active_set = (func(x, y, theta0))
 
+    while np.sum(r**2>tol):
 
-
-def gaussian_kernel(x, *params):
-    """
-    A multi-dimensional Gaussian kernel function
-
-    Useful for creating and testing EBP with simple Gaussian Mixture Models
-
-    Parameters
-    ----------
-    x : ndarray
-        The independent variable over which the Gaussian is calculated
-
-    """
-    mu = np.asarray(params[0])
-    sigma = np.asarray(params[1])
-    if mu.shape[0] != sigma.shape[0]:
-        raise ValueError("Inputs mu and sigma should have the same dimensions")
-    else:
-        dims = mu.shape[0]
         
-    if len(sigma.shape) == 1:
-        sigma = np.diag(sigma)
-    elif len(sigma.shape) == 2:
-        pass
-    else:
-        e_s = "Input sigma should be a 1D array (for diagonal "
-        e_s += "covariance matrix), or 2D (for non-diagonal covariance) matrix"
-        raise ValueError(e_s)
+        r = active_set[-1] - y
+        
+        regressors = np.hstack(active_set)
+    
 
-    while len(mu.shape) < len(x.shape): 
-        mu = mu[..., None]
-
-    shape_tuple = x.shape[1:]
-    diff = (x - mu).reshape(x.shape[0], -1)
-    sigma_inv = nla.inv(sigma)
-    mult1 = np.dot(diff.T, sigma_inv)
-    mult2 =  (np.diag(np.dot(mult1, diff))).reshape(shape_tuple)
-    norm_factor = 1/(np.sqrt((2*np.pi)**dims * nla.det(sigma)))
-    gauss = norm_factor * np.exp(-0.5 * mult2) 
-    return gauss
--- tests/test_ebp.py
+++ tests/test_ebp.py
@@ -12,7 +12,7 @@ def test_gaussian_kernel():
     # Variance/covariance matrix:
     sigma3d = np.array([[10, 10, 0], [0,10, 10], [10,10,10]])
 
-    gauss3d = ebp.gaussian_kernel(xx3d, mean3d, sigma3d)
+    gauss3d = ebp.gaussian_kernel(xx3d, np.hstack([mean3d, sigma3d.ravel()]))
 
     # Verify against a 1D case:
     x = xx3d[0,0,:,0]
@@ -20,9 +20,38 @@ def test_gaussian_kernel():
                 * np.exp(-0.5 * (x - mean3d[0]) **2 / sigma3d[0,0]))
 
     # should be identical up to a scaling factor:
-
-    ratio = gauss3d[10,10]/gauss1d
+    ratio = gauss3d[10, 10]/gauss1d
     should_be_ones = ratio / np.mean(ratio)
     npt.assert_almost_equal(should_be_ones, np.ones(should_be_ones.shape))
     
-    
+
+def test_gaussian_oracle():
+    mean1 = [20, 20]
+    sigma1 = [10, 10]
+    params1 = np.hstack([mean1, sigma1])
+
+    xx2d = np.array(np.meshgrid(np.arange(-100,100,10),
+                                np.arange(-100,100,10))).reshape(2, -1)
+
+    gauss2d = ebp.gaussian_kernel(xx2d, params1)
+    # Oh, we're totally stuffing this ballot box:
+    initial  = params1 
+    theta_est = ebp.leastsq_oracle(xx2d, gauss2d, ebp.gaussian_kernel, initial,
+                               bounds=[[None, None], [None, None], [0, None],
+                                       [0, None]]) # Variance can't be negative!
+
+    npt.assert_almost_equal(theta_est, params1)
+                                
+
+
+##     # Make a 2D mixture of 2 Gaussians:
+##     mean1 = [20, 20]
+##     sigma1 = [10, 10]
+##     kernel1 = ebp.gaussian_kernel(xx2d, mean1, sigma1)
+##     mean2 = [30, 40]
+##     sigma2 = [10, 50]
+##     kernel2 = ebp.gaussian_kernel(xx2d, mean2, sigma2)
+##     beta = [0.3, 0.7]
+##     signal = beta[0] * kernel1 + beta[1] * kernel2
+
+##     ebp.elastic_basis_pursuit(xx2d, signal, gaussian_oracle, gaussian_kernel)
